a933c9747e11650c9af42d1cc44e3b8475523c78
Jagjot Singh <jagjotsinghlko@gmail.com>
2014-04-24 11:45:46 +0530

    This program deals with the Calculation of Inverse based on Cached Data for the matrix present or not

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,28 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function()x
+  setinv<-function(cacheSolve)m<<-cacheSolve
+  getinv<-function()m
+  list(set=set,get=get,setinv=setinv,getinv=getinv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse of 'x'
+m1<-x$getinv()
+if(!is.null(m1)){
+  return(m1)
+}
+data<-x$get()
+m1<-solve(data)
+x$setinv(m1)
+m1
 }
